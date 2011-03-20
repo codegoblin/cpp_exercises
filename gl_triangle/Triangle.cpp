799cc6d1098d2b5ebba4a88f9dc5b60369caaf66
Cody Johnston <cody@metroenterprises.us>
2011-03-20 03:33:37 -0700

    Triangle now renders the tri-force

--- gl_triangle/Triangle.cpp
+++ gl_triangle/Triangle.cpp
@@ -1,11 +1,6 @@
 //
 //  Triangle.cpp
-//  Draws a Single Triangle using OpenGL
-//
-//  Required libraries:
-//  - OpenGL
-//  - GLUT
-//  - GLtools
+//  Triangle
 //
 //  Created by Cody Johnston on 3/19/11.
 //  No Copyright. Just use it dawg.
@@ -22,6 +17,8 @@
 #endif
 
 GLBatch triangleBatch;
+GLBatch triangleBatch2;
+GLBatch triangleBatch3;
 GLShaderManager shaderManager;
 
 void ChangeSize( int w, int h )
@@ -31,30 +28,52 @@ void ChangeSize( int w, int h )
 
 void SetupRC( )
 {
-  //Blue Background
-  glClearColor( 0.0f, 0.0f, 1.0f, 1.0f );
+  //Black Background
+  glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 
   shaderManager.InitializeStockShaders( );
 
   // Load up a triangle
   GLfloat vVerts[] =  { 
-                        -0.5f, 0.0f, 0.0f,
-                        0.5f, 0.0f, 0.0f,
-                        0.0f, 0.5f, 0.0f
+    -0.5f, 0.0f, 0.0f,
+    0.5f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+  };
+  
+  GLfloat vVerts2[] =  { 
+    -1.0f, -1.0f, 0.0f,
+    0.0f, -1.0f, 0.0f,
+    -0.5f, 0.0f, 0.0f
+  };
+  
+  GLfloat vVerts3[] =  { 
+    0.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.5f, 0.0f, 0.0f
   };
 
   triangleBatch.Begin( GL_TRIANGLES, 3 );
   triangleBatch.CopyVertexData3f( vVerts );
   triangleBatch.End( );
+  
+  triangleBatch2.Begin( GL_TRIANGLES, 3 );
+  triangleBatch2.CopyVertexData3f( vVerts2 );
+  triangleBatch2.End( );
+  
+  triangleBatch3.Begin( GL_TRIANGLES, 3 );
+  triangleBatch3.CopyVertexData3f( vVerts3 );
+  triangleBatch3.End( );
 }
 
 void RenderScene( void )
 {
   // Clear the window with current clearing color
   glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
-  GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-  shaderManager.UseStockShader( GLT_SHADER_IDENTITY, vRed );
+  GLfloat vYellow[] = { 5.0f, 5.0f, 0.0f, 1.0f };
+  shaderManager.UseStockShader( GLT_SHADER_IDENTITY, vYellow );
   triangleBatch.Draw( );
+  triangleBatch2.Draw( );
+  triangleBatch3.Draw( );
   
   // Buffer swap and display back buffer
   glutSwapBuffers( );
